c9e40128105ae6de1af29f341cba13b8b054b367
Inflaton <inflaton.sg@gmail.com>
2021-01-21 12:12:10 +0800

    load menu via JS

--- pages/member/js/home.js
+++ pages/member/js/home.js
@@ -17,37 +17,85 @@ function setupCheckbox(element, pathname, forLineage) {
 		});
 	}
 }
+function loadNavigationMenu(el) {
+	const url = el.getAttribute('data-include');
+	var localTest = /^(?:file):/,
+		xmlhttp = new XMLHttpRequest(),
+		status = 0;
+
+	xmlhttp.onreadystatechange = function () {
+		/* if we are on a local protocol, and we have response text, we'll assume
+*  				things were successful */
+		if (xmlhttp.readyState == 4) {
+			status = xmlhttp.status;
+		}
+		if (localTest.test(location.href) && xmlhttp.responseText) {
+			status = 200;
+		}
+		if (xmlhttp.readyState == 4 && status == 200) {
+			var html = xmlhttp.responseText;
+			const pathname = window.location.pathname.replace("/amitabha", "");
+			var array = pathname.split("/");
+			const count = array.length - 2;
+			for (var j = 0; j < count; j++) {
+				if (j == 0) {
+					html = html.replaceAll('href=".', 'href="..')
+				} else {
+					html = html.replaceAll('href=".', 'href="../.')
+				}
+			}
+			// console.log(`html: ${html}`);
+			el.outerHTML = html;
+		}
+
+		const loggedInUser = Parse.User.current();
+		// console.log(
+		// 	`loggedInUser: ${JSON.stringify(loggedInUser)}`
+		// );
+
+		var element = document.getElementById(loggedInUser ? 'member-func' : 'non-member-func');
+
+		if (element) {
+			element.setAttribute('style', 'display: block;');
+		}
+		element = document.getElementById('logoutButton2');
+
+		if (element) {
+			element.addEventListener('click', () => {
+				Parse.User.logOut().catch(() => { }).then(() => {
+					window.location.href = '/index.html';
+					return false;
+				});
+			}, false);
+		}
+	}
+
+	try {
+		xmlhttp.open("GET", url, true);
+		xmlhttp.send();
+	} catch (err) {
+		/* todo catch error */
+	}
+}
 
 document.addEventListener('DOMContentLoaded', () => {
+	var element = document.getElementById('navigationMenu');
+	if (element) {
+		loadNavigationMenu(element);
+	}
+
 	const loggedInUser = Parse.User.current();
 	// console.log(
 	// 	`loggedInUser: ${JSON.stringify(loggedInUser)}`
 	// );
 
-	var element = document.getElementById(loggedInUser ? 'member' : 'non-member');
-
-	if (element) {
-		element.setAttribute('style', 'display: block;');
-	}
-
-	element = document.getElementById(loggedInUser ? 'member-func' : 'non-member-func');
+	element = document.getElementById(loggedInUser ? 'member' : 'non-member');
 
 	if (element) {
 		element.setAttribute('style', 'display: block;');
 	}
 
 	element = document.getElementById('logoutButton');
-
-	if (element) {
-		element.addEventListener('click', () => {
-			Parse.User.logOut().catch(() => { }).then(() => {
-				window.location.href = '/index.html';
-				return false;
-			});
-		}, false);
-	}
-
-	element = document.getElementById('logoutButton2');
 
 	if (element) {
 		element.addEventListener('click', () => {
