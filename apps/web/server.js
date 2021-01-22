34a4188fe6470ef2f5eedcec846ec6346feed262
Inflaton <inflaton.sg@gmail.com>
2021-01-22 17:59:57 +0800

    force https

--- apps/web/server.js
+++ apps/web/server.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
+const { env } = require("process");
+
+const nodeEnv = process.env.NODE_ENV || "development";
+console.log(`nodeEnv: ${nodeEnv}`);
+
+var forceSSL = function(req, res, next) {
+  if (req.headers["x-forwarded-proto"] !== "https") {
+    return res.redirect(["https://", req.get("Host"), req.url].join(""));
+  }
+  return next();
+};
 
 const app = express();
+
+if (nodeEnv !== "development") {
+  console.log(`env: ${JSON.stringify(env)}`);
+  app.use(forceSSL);
+}
 
 //here we are configuring dist to serve app files
 app.use("/", serveStatic(path.join(__dirname, "/../../pages")));
